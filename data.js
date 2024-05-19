80fc3ce13285546a56f42d64f7b47fc71a61fcad
Sudarik1 <olonichevy@gmail.com>
2024-05-19 19:24:20 +0400

    Styles improved

--- data.js
+++ data.js
@@ -1,6 +1,5 @@
 export const notes = [
     { title: `Гуманистическая астрология`, 
-      description: `Description of Note 1`, 
       paragraph: `Почему это сайт озаглавлен "Гуманистическая астрология"
       Этот термин впервые начал использовть американский астролог
       Дэйн Радьяр в 60-е годы 20 века с целью произвести сдвиг в
@@ -26,37 +25,26 @@ export const notes = [
       богатой и осмысленной жизнью, стать подлинным хозяином своей
       судьбы.`},
     { title: `Note 2`,
-      description: `Description of Note 2`,
       paragraph: `Suspendisse sit amet vehicula odio. Maecenas ac augue a libero congue molestie eu ut orci.` },
     { title: `Note 3`,
-      description: `Description of Note 3`,
       paragraph: `Vestibulum vel gravida dolor. Nulla euismod ligula at ante vehicula, nec lobortis justo hendrerit.` },
     { title: `Note 4`,
-      description: `Description of Note 4`,
       paragraph: `Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.` },
     { title: `Note 5`,
-      description: `Description of Note 5`,
       paragraph: `Fusce nec odio nec nunc vehicula tincidunt sit amet sed elit. Nunc et nisl at libero vehicula ullamcorper.` },
     { title: `Note 1`,
-      description: `Description of Note 1`,
       paragraph: `Proin pretium lectus at ante cursus, et consectetur felis egestas. Nulla facilisi. Nam sed purus condimentum, laoreet velit nec, lacinia nulla.` },
     { title: `Note 2`, 
-      description: `Description of Note 2`,
       paragraph: `Aliquam ac elit eget velit rutrum suscipit at nec nulla. Vestibulum et lacus nec arcu gravida venenatis.` },
     { title: `Note 3`, 
-      description: `Description of Note 3`,
       paragraph: `Phasellus tempus elit non ultricies ullamcorper. Phasellus vehicula vel risus non convallis.` },
     { title: `Note 4`, 
-      description: `Description of Note 4`,
       paragraph: `Curabitur sit amet ex fermentum, suscipit nunc eget, mattis odio. Aenean consectetur metus vitae turpis tempor, sed accumsan justo gravida.` },
     { title: `Note 5`,
-      description: `Description of Note 5`,
       paragraph: `Etiam eu lorem non justo viverra suscipit. Integer ac tellus vestibulum, aliquam mi sit amet, tincidunt ante.` },
     { title: `Note 5`,
-      description: `Description of Note 5`,
       paragraph: `Etiam eu lorem non justo viverra suscipit. Integer ac tellus vestibulum, aliquam mi sit amet, tincidunt ante.` },
     { title: `Note 5`,
-      description: `Description of Note 5`,
       paragraph: `Etiam eu lorem non justo viverra suscipit. Integer ac tellus vestibulum, aliquam mi sit amet, tincidunt ante.` },
 ];
 
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 
 import { notes } from "./data.js";
 
+
+// grab application form 
+const applicationForm = document.getElementById('application-form')
+console.log(applicationForm)
+
 // grab "pages" content 
 const toggleContentDisplayBtnArray = document.querySelectorAll('.nav-btn');
 
+// grab dimploms photos
+const dimplomPhotoArray = document.querySelectorAll('.diplom-photo')
+
 // grab of notes
 const notesList = document.getElementById("notes-list");
-
-const fullNoteContainer = document.getElementById("fullNoteContainer");
-
+const fullNoteContainer = document.getElementById("full-note-container");
 const backToAllNotesBtn = document.getElementById('back-to-all-notes-btn')
 
- 
+
 // display "pages" content 
 
 // add click event listener to each toggle button
 toggleContentDisplayBtnArray.forEach(button => {
     button.addEventListener('click', toggleContentDisplay);
 });
-
-backToAllNotesBtn.addEventListener('click', showAllNotes)
-
 
 function toggleContentDisplay(e) {
     // Get the target section ID from the button's data attribute
@@ -39,39 +42,47 @@ function toggleContentDisplay(e) {
     }
 }
 
+// expand dimploms photos
 
+dimplomPhotoArray.forEach(photo =>{
+    photo.addEventListener('click', ()=> {
+        if (photo.classList.contains('expanded')){
+            photo.classList.remove('expanded')
+        }
+        else {
+            dimplomPhotoArray.forEach(photo => photo.classList.remove('expanded'))
+            photo.classList.add('expanded')
+        }
+        
+    })
+})
 // display notes
-const notesPerPage = 20;
-let currentPage = 1;
-const startIndex = (currentPage - 1) * notesPerPage;
-const endIndex = startIndex + notesPerPage;
 
 function getFeedNotesListHtml(){
     let feedHtml = ''
-    for (let i = startIndex; i < endIndex && i < notes.length; i++) {
+    for (let i = 0; i < notes.length; i++) {
         feedHtml += `
-        <div class='rendered-note'>
-            <h2 class="note-title ">${notes[i].title}</h2>
+        <div class='rendered-note note-link-style'>
+            <h2 class="note-title notes-font-weight">${notes[i].title}</h2>
         </div>
         `
-        // <p class="">${notes[i].description}</P>
     }
     return feedHtml
+}
+
+function displayNotes() {
+    notesList.innerHTML = getFeedNotesListHtml()
+    addEventListenerToNotes();
 }
 
 function addEventListenerToNotes(){
     const noteTitles = document.querySelectorAll('.note-title');
     noteTitles.forEach(title => {
         title.addEventListener('click', function() {
-            const index = Array.from(noteTitles).indexOf(title) + startIndex;
+            const index = Array.from(noteTitles).indexOf(title);
             displayFullNote(notes[index]);
         });
     });
-}
-
-function displayNotes() {
-    notesList.innerHTML = getFeedNotesListHtml()
-    addEventListenerToNotes();
 }
 
 displayNotes();
@@ -81,7 +92,7 @@ displayNotes();
 function getFeedFullNoteHtml(note){
     let feedHtml = `
         <div id='full-note-container'>
-            <h2>${note.title}</h2>
+            <h2 class='note-title'>${note.title}</h2>
             <p>${note.paragraph}</p>
         </div>
         `
@@ -99,6 +110,10 @@ function hideNotes(){
     renderedNotes.forEach( (note) => note.classList.add('hidden')) 
 }
 
+// return to all notes
+
+backToAllNotesBtn.addEventListener('click', showAllNotes)
+
 function showAllNotes(){
     const renderedNotes = document.querySelectorAll('.rendered-note');
     renderedNotes.forEach( (note) => note.classList.remove('hidden'))
@@ -110,3 +125,4 @@ function hideFullNote(){
     const fullNote = document.getElementById('full-note-container')
     fullNote.classList.add('hidden')
 }
+
